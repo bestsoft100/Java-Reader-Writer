581671a178737a929677a37efbb3898886374615
bestsoft100 <49400900+bestsoft100@users.noreply.github.com>
2022-03-21 18:45:43 +0100

    Fixed Number Parsing

--- src/b100/json/element/JsonNumber.java
+++ src/b100/json/element/JsonNumber.java
@@ -1,5 +1,7 @@
 package b100.json.element;
 
+import static b100.utils.Utils.*;
+
 import b100.utils.InvalidCharacterException;
 import b100.utils.StringReader;
 import b100.utils.StringWriter;
@@ -9,7 +11,7 @@ public class JsonNumber implements JsonElement{
 	public Number value;
 	
 	public JsonNumber(Number number) {
-		this.value = number;
+		this.value = requireNonNull(number);
 	}
 	
 	public JsonNumber(int i) {
@@ -29,57 +31,120 @@ public class JsonNumber implements JsonElement{
 	}
 	
 	public JsonNumber(StringReader reader) {
+		//https://www.json.org/json-en.html
 		StringWriter numberString = new StringWriter();
-		boolean isDecimal = false;
-		boolean isLong = false;
 		
-		while(true) {
-			char c = reader.get();
-			
-			if(c >= '0' && c <= '9') {
-				numberString.write(c);
-				reader.next();
-			} else if(c == '.') {
-				isDecimal = true;
-				numberString.write(c);
-				reader.next();
-			} else if(c == '-') {
-				numberString.write(c);
-				reader.next();
-			} else if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-				if((isDecimal && (c == 'd' || c == 'D')) || (!isDecimal && (c == 'l' || c == 'L'))) {
-					isLong = true;
-				}
-				numberString.write(c);
-				reader.next();
-			} else if(reader.isWhitespace(c) || c == ',' || c == '}') {
-				break;
-			} else {
-				throw new InvalidCharacterException(reader);
-			}
+		boolean decimal = false;
+		
+		if(reader.get() == '-') {
+			numberString.write(reader.get());
+			reader.next();
 		}
 		
-		String str = numberString.toString();
-		if(isDecimal) {
-			if(isLong) {
-				value = Double.parseDouble(str);
+		readInteger(reader, numberString);
+		if(reader.get() == '.') {
+			readDecimal(reader, numberString);
+			decimal = true;
+		}
+		
+		if(reader.get() == 'e' || reader.get() == 'E') {
+			readExponent(reader, numberString);
+		}
+		
+		if(reader.get() == ',' || reader.get() == '}' || reader.isWhitespace(reader.get())) {
+			String str = numberString.toString();
+			
+			if(decimal) {
+				float floatVal = 0.0f;
+				double doubleVal = 0.0;
+				
+				try {
+					floatVal = Float.parseFloat(str);
+				}catch (Exception e) {}
+				doubleVal = Double.parseDouble(str);
+				
+				if(floatVal == doubleVal) {
+					value = floatVal;
+				}else {
+					value = doubleVal;
+				}
 			}else {
-				value = Float.parseFloat(str);
+				int intVal = 0;
+				long longVal = 0L;
+				
+				try {
+					intVal = Integer.parseInt(str);
+				}catch (Exception e) {}
+				longVal = Long.parseLong(str);
+				
+				if(intVal == longVal) {
+					value = intVal;
+				}else {
+					value = longVal;
+				}
 			}
 		}else {
-			if(isLong) {
-				value = Long.parseLong(str);
+			throw new InvalidCharacterException(reader);
+		}
+	}
+	
+	private void readInteger(StringReader reader, StringWriter numberString) {
+		while(true) {
+			if(reader.get() == '0') {
+				numberString.write(reader.get());
+				reader.next();
+				return;
+			}else if(reader.get() >= '1' && reader.get() <= '9') {
+				numberString.write(reader.get());
+				reader.next();
+				while(true) {
+					if(reader.get() >= '0' && reader.get() <= '9') {
+						numberString.write(reader.get());
+						reader.next();
+					}else {
+						return;
+					}
+				}
 			}else {
-				value = Integer.parseInt(str);
+				return;
 			}
 		}
 	}
-
+	
+	private void readDecimal(StringReader reader, StringWriter numberString) {
+		reader.expectAndSkip('.');
+		while(true) {
+			if(reader.get() >= '0' && reader.get() <= '9') {
+				numberString.write(reader.get());
+				reader.next();
+			}else {
+				return;
+			}
+		}
+	}
+	
+	private void readExponent(StringReader reader, StringWriter numberString) {
+		reader.expectOne("eE");
+		numberString.write(reader.get());
+		reader.next();
+		
+		if(reader.get() == '+' || reader.get() == '-') {
+			numberString.write(reader.get());
+			reader.next();
+		}
+		
+		while(true) {
+			if(reader.get() >= '0' && reader.get() <= '9') {
+				numberString.write(reader.get());
+				reader.next();
+			}else {
+				break;
+			}
+		}
+	}
+	
 	public void write(StringWriter writer) {
 		writer.write(value.toString());
-		
-		if(value instanceof Float) writer.write('f');
-		if(value instanceof Long) writer.write('l');
 	}
 	
 	public int getInteger() {
@@ -107,7 +172,7 @@ public class JsonNumber implements JsonElement{
 	}
 	
 	public void set(Number n) {
-		this.value = n;
+		this.value = requireNonNull(n);
 	}
 	
 	public void set(int i) {
@@ -124,6 +189,22 @@ public class JsonNumber implements JsonElement{
 	
 	public void set(double d) {
 		this.value = d;
+	}
+	
+	public boolean isInteger() {
+		return this.value instanceof Integer;
+	}
+	
+	public boolean isLong() {
+		return this.value instanceof Long;
+	}
+	
+	public boolean isFloat() {
+		return this.value instanceof Float;
+	}
+	
+	public boolean isDouble() {
+		return this.value instanceof Double;
 	}
 	
 }
